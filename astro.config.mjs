a6763bc73320baac7afa742da52c98292c7a3e94
Mike Mali <michaeltdmali@gmail.com>
2025-06-17 17:51:35 -0400

    Updated vite config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,17 +5,6 @@ import { defineConfig } from 'astro/config'
 export default defineConfig({
   vite: {
     // Example: Add custom vite plugins directly to your Astro project
-    plugins: [
-      {
-        name: 'configure-response-headers',
-        configureServer: (server) => {
-          server.middlewares.use((_req, res, next) => {
-            res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp')
-            res.setHeader('Cross-Origin-Opener-Policy', 'same-origin')
-            next()
-          })
-        },
-      },
-    ],
+    plugins: [],
   },
 })
