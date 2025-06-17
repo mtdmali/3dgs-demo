61fe3ae782b0fcb24260ebc5536598ef6882a869
Mike Mali <michaeltdmali@gmail.com>
2025-06-17 17:50:26 -0400

    Updated vite config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
-import crossOriginIsolation from 'vite-plugin-cross-origin-isolation'
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     // Example: Add custom vite plugins directly to your Astro project
-    plugins: [crossOriginIsolation()],
+    plugins: [
+      {
+        name: 'configure-response-headers',
+        configureServer: (server) => {
+          server.middlewares.use((_req, res, next) => {
+            res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp')
+            res.setHeader('Cross-Origin-Opener-Policy', 'same-origin')
+            next()
+          })
+        },
+      },
+    ],
   },
 })
